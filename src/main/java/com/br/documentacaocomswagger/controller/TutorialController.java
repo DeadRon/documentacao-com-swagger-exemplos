234e107158a7377fdc9726ed2b8de08eb87f1ab2
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-21 15:27:25 -0300

    [DOCS] - @ApiResponses anotacao para espeficar o conjuntos de possiveis respostas da api de buscar tutorial por id(cada item do conjunto de respostas é representado pela anotacao @ApiResponse. @ApiResponse especifica cada possivel resposta da api

--- src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
+++ src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
@@ -2,6 +2,10 @@ package com.br.documentacaocomswagger.controller;
 
 import com.br.documentacaocomswagger.config.openapi.model.Tutorial;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +19,10 @@ public class TutorialController {
             summary = "Retrieve a Tutorial by Id",
             description = "Get a Tutorial object by specifying its id. The response is Tutorial object with id, title, description and published status."
     )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Tutorial.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
         return null;
