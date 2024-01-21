a272e5e2908d03e9e6b5c12282eeef6d8fc4e2f4
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-21 14:59:45 -0300

    [DOCS] - tag do swagger que agrupa as operacoes de api disponiveis no tutorial controller

--- src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
+++ src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
@@ -1,8 +1,9 @@
 package com.br.documentacaocomswagger.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "Tutorial - Controller", description = "Tutorial management APIs")
 @RestController
 public class TutorialController {
 
