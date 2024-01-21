1db020b5c0b73f90ca2362aefbce8d50e39e2921
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-21 15:00:49 -0300

    [FEAT] - buscar um tutorial por id

--- src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
+++ src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
@@ -1,10 +1,19 @@
 package com.br.documentacaocomswagger.controller;
 
+import com.br.documentacaocomswagger.config.openapi.model.Tutorial;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Tutorial - Controller", description = "Tutorial management APIs")
 @RestController
 public class TutorialController {
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
+        return null;
+    }
 
 }
