eda2f1d519390c85f7676dc05347f8dcda5f05dd
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-21 15:02:59 -0300

    [DOCS] - forcener informacoes/metadados da api que busca um tutorial por id

--- src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
+++ src/main/java/com/br/documentacaocomswagger/controller/TutorialController.java
@@ -1,6 +1,7 @@
 package com.br.documentacaocomswagger.controller;
 
 import com.br.documentacaocomswagger.config.openapi.model.Tutorial;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Tutorial - Controller", description = "Tutorial management APIs")
 @RestController
 public class TutorialController {
-
+    @Operation(
+            summary = "Retrieve a Tutorial by Id",
+            description = "Get a Tutorial object by specifying its id. The response is Tutorial object with id, title, description and published status."
+    )
     @GetMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
         return null;
